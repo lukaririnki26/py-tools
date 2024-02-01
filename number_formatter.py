8fa5112c65dd60edf14fc7c6ef5feea4cba949ff
fahmi <fahmiriottss@gmail.com>
2024-02-01 14:12:08 +0800

    update: add tuple

--- number_formatter.py
+++ number_formatter.py
@@ -4,14 +4,15 @@ from flask import Blueprint, render_template, request
 number_formatter = Blueprint('number_formatter', __name__)
 
 def format_number(input_number, format_type):
-    if format_type == 'decimal':
-        return f"{input_number:,}"
+    if format_type == 'tupple':
+        data_list = input_number.split()
+        formatted_data = ', '.join(data_list)
+        return f"({formatted_data})"
     elif format_type == 'binary':
         return bin(int(input_number))[2:]
-    elif format_type == 'hexadecimal':
-        return hex(int(input_number))[2:]
     else:
         return "Invalid format type for number."
+
 
 @number_formatter.route('/', methods=['GET', 'POST'])
 def number_formatter_route():
@@ -19,7 +20,7 @@ def number_formatter_route():
 
     if request.method == 'POST':
         try:
-            input_number = float(request.form['input_number'])
+            input_number = request.form['input_number']
             format_type = request.form['format_type']
             number_result = format_number(input_number, format_type)
 
