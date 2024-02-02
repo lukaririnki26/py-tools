da481912c492c20365971759e7f55b0d1f571a90
fahmi <fahmiriottss@gmail.com>
2024-02-03 03:03:05 +0800

    add: html table toexcel

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.finder import finder
 from src.formatter import formatter
 from src.excel_to_json import excel_to_json
 from src.json_to_excel import json_to_excel
+from src.html_table_to_excel import html_table_to_excel
 
 app = Flask(__name__)
 
@@ -11,10 +12,11 @@ app.register_blueprint(finder, url_prefix='/finder')
 app.register_blueprint(formatter, url_prefix='/formatter')
 app.register_blueprint(excel_to_json, url_prefix='/excel_to_json')
 app.register_blueprint(json_to_excel, url_prefix='/json_to_excel')
+app.register_blueprint(html_table_to_excel, url_prefix='/html_table_to_excel')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port= 50002)
---
+++ src/html_table_to_excel.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, render_template, request, send_file
+import pandas as pd
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from io import BytesIO
+import requests
+
+html_table_to_excel = Blueprint('html_table_to_excel', __name__)
+
+def fetch_html_content(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for bad status codes
+    return response.text
+
+def _html_table_to_excel(html_content, table_id, sheet_name='converted_table'):
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    table = soup.find(id=table_id)
+    print(table)
+    if not table:
+        raise ValueError("No table found in HTML content")
+
+    # Read HTML table into a DataFrame
+    df = pd.read_html(str(table))[0]
+
+    # Create a workbook and add a worksheet
+    output = BytesIO()
+    workbook = Workbook()
+    worksheet = workbook.active
+
+    # Write DataFrame to worksheet
+    for row in dataframe_to_rows(df, index=False, header=True):
+        worksheet.append(row)
+
+    # Ensure at least one sheet is visible
+    worksheet.sheet_state = 'visible'
+
+    # Save the modified workbook
+    workbook.save(output)
+    output.seek(0)
+    return output
+
+@html_table_to_excel.route('/', methods=['GET'])
+def index():
+    return render_template('html_table_to_excel.html')
+
+@html_table_to_excel.route('/', methods=['POST'])
+def convert():
+    url = request.form.get('url', '')
+    table_id = request.form.get('table_id', '')
+
+    try:
+        html_content = fetch_html_content(url)
+        excel_output = _html_table_to_excel(html_content, table_id,  sheet_name='converted_table')
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+    return send_file(excel_output, download_name='converted_table.xlsx', as_attachment=True)
