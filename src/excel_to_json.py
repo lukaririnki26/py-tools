af4e17cb91deefb80740ab38875a9c915213e2b3
luka ririnki <fahmiriottss@gmail.com>
2024-07-08 13:50:41 +0700

    excel to json only file

--- src/excel_to_json.py
+++ src/excel_to_json.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify, send_file
 import pandas as pd
-import os
 import io
 import json
 
@@ -13,42 +12,45 @@ def index():
 @excel_to_json.route('/', methods=['POST'])
 def convert():
     if 'file' not in request.files:
-        error = f"Error: No File Part"
+        error = "Error: No File Part"
         return render_template('excel_to_json.html', error=error)
 
     file = request.files['file']
 
     if file.filename == '':
-        error = f"Error: No Selected File"
+        error = "Error: No Selected File"
+        return render_template('excel_to_json.html', error=error)
+
+    custom_keys = request.form.get('custom_keys')
+    try:
+        custom_keys = json.loads(custom_keys) if custom_keys else {}
+    except json.JSONDecodeError as e:
+        error = f"Error: Invalid JSON format for custom keys. {str(e)}"
         return render_template('excel_to_json.html', error=error)
 
     try:
         # Read Excel file into a DataFrame
         df = pd.read_excel(file, engine='openpyxl')
 
-        # Convert DataFrame to JSON
-        result = df.to_dict(orient='records')
+        # Replace NaN with None (which will be converted to null in JSON)
+        df = df.where(pd.notnull(df), None)
 
-        return render_template('excel_to_json.html', result=result)
+        # Create a custom JSON structure with custom keys or default to column names
+        result = []
+        for _, row in df.iterrows():
+            item = {}
+            for col in df.columns:
+                custom_key = custom_keys.get(col, col)  # Use custom key if available, otherwise default to column name
+                item[custom_key] = row[col]
+            result.append(item)
 
-    except Exception as e:
-        error = f"Error: {str(e)}"
-        return render_template('excel_to_json.html', error=error)
-
-@excel_to_json.route('/download', methods=['GET', 'POST'])
-def download():
-    data = request.form.get('data')
-
-    if not data:
-        error = f"Error: No Data Provided"
-        return render_template('excel_to_json.html', error=error)
-
-    try:
-        data_json = json.loads(data)
-        json_str = json.dumps(data_json, indent=2, ensure_ascii=False)
+        # Convert JSON to string and write to a BytesIO object
+        json_str = json.dumps(result, indent=2, ensure_ascii=False)
         output = io.BytesIO()
         output.write(json_str.encode('utf-8'))
         output.seek(0)
+
+        # Send the JSON file as a download
         return send_file(output, as_attachment=True, download_name='output.json', mimetype='application/json')
 
     except Exception as e:
